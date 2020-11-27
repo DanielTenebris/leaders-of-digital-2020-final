244b5ae27af1af909b9773019cd04d0327896ec6
niqitosiq <shtanko@insigne.ru>
2020-11-27 21:03:41 +0400

    Permisions reset

---
+++ frontend/components/microphone.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="microphone" @click="start">начать стрим</div>
+</template>
+
+<script>
+import WSAudioApi from 'ws-audio-api';
+
+export default {
+  name: 'microphone',
+
+  data() {
+    return {
+      streamer: null,
+    };
+  },
+
+  methods: {
+    start() {
+      this.streamer = WSAudioApi.Streamer({});
+    },
+  },
+};
+</script>
+
+<style></style>
