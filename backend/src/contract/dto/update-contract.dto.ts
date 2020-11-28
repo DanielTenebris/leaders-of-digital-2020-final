566509658a8dfd8ed51644c57979f67f9de9857f
DanielTenebris <danieltenebris@gmail.com>
2020-11-28 10:41:21 +0000

    :recycle: update dto, entity

--- backend/src/contract/dto/update-contract.dto.ts
+++ backend/src/contract/dto/update-contract.dto.ts
@@ -2,11 +2,14 @@ import { IsString } from "class-validator";
 
 export class UpdateContractDto {
     @IsString()
-    script: string;
+    readonly script: string;
 
     @IsString()
-    workspace: string;
+    readonly workspace: string;
 
     @IsString()
-    toolbox: string;
+    readonly toolbox: string;
+
+    @IsString()
+    readonly label: string;
 }
