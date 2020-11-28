51a58190fca040fcb0da7b7b4375f6eb7cb19aee
DanielTenebris <danieltenebris@gmail.com>
2020-11-28 20:04:42 +0000

    :construct: Working for contrarcts

--- backend/src/contract/contract.service.ts
+++ backend/src/contract/contract.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { TaskEntity } from 'src/task/entities/task.entity';
 import { Repository, DeepPartial } from 'typeorm';
 import { CreateContractDto } from './dto/create-contract.dto';
 import { UpdateContractDto } from './dto/update-contract.dto';
@@ -11,7 +10,7 @@ import { ContractNotFoundException } from './exceptions/contract.exceptions';
 export class ContractService {
     constructor(
         @InjectRepository(ContractEntity)
-        private readonly contractRepository: Repository<ContractEntity>,
+        private readonly contractRepository: Repository<ContractEntity>
     ) {}
 
     async create(contract: CreateContractDto): Promise<ContractEntity> {
@@ -39,26 +38,5 @@ export class ContractService {
     async deleteOne(contractId: number): Promise<ContractEntity[]> {
         const contractToRemove = await this.contractRepository.find({id: contractId});
         return await this.contractRepository.remove(contractToRemove);
-    }
-
-    async execContracts(taskId: number): Promise<any> {
-        
-        const dateNow = Date.now()
-        const task = this.findOneById(taskId)
-        console.log(task)
-        const deadline = task["deadline"]
-        
-        const emitEvents = []
-        const contracts = await this.contractRepository.find({ relations: ['tasks'] } );
-        console.log(`UPDATE !!!!!! ${contracts}`);
-
-        (await contracts).forEach( (contract) => {
-            console.log(`UPDATE !!!!!! ${contract}`);
-            // eval(contract["script"])
-        })
-        console.log(emitEvents)
-        
-        return true;
-
     }
 }
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { classToPlain } from 'class-transformer';
 import { ContractService } from 'src/contract/contract.service';
 import { Repository, DeepPartial } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,8 +13,9 @@ export class TaskService {
     constructor(
         @InjectRepository(TaskEntity)
         private readonly taskRepository: Repository<TaskEntity>,
-        
+
         private readonly contractService: ContractService,
+
     ) {}
 
     async create(task: CreateTaskDto): Promise<TaskEntity> {
@@ -38,7 +40,7 @@ export class TaskService {
         await this.taskRepository.update({id: taskId}, updateTask);
         const updatedTask = await this.taskRepository.findOne(taskId);
         if (updatedTask) {
-            this.contractService.execContracts(taskId)
+            this.execContracts(taskId)
             return updatedTask
         } 
         throw new TaskNotFoundException(taskId);
@@ -47,5 +49,27 @@ export class TaskService {
     async deleteOne(taskId: number): Promise<TaskEntity[]> {
         const taskToRemove = await this.taskRepository.find({id: taskId});
         return await this.taskRepository.remove(taskToRemove);
+    }
+
+    async execContracts(taskId: number): Promise<any> {
+        
+        console.log(`TASK SUKA ID ->>>> ${taskId}`)
+        
+        
+        const task = this.findOneById(taskId)
+        const emitEvents = []
+        const dateNow = Date.now()
+        const deadline = task["deadline"]
+        
+        
+        const taskWithRelatedContracts = await this.taskRepository.findOne({ where: {id: taskId}, relations: ["contracts"]});
+
+        const contracts = classToPlain(taskWithRelatedContracts)["contracts"]
+        
+        contracts.forEach( (contract) => {
+            eval(contract["script"])
+        })
+    
+        console.log(emitEvents)
     }
 }
