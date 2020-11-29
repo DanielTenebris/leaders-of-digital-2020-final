9c319c94fa4f45d6ef8e6f499f1dfa57d6c43dae
niqitosiq <shtanko@insigne.ru>
2020-11-29 04:14:52 +0400

    Create task event

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -10,77 +10,98 @@ import { TaskNotFoundException } from './exceptions/task.exceptions';
 
 @Injectable()
 export class TaskService {
-    constructor(
-        @InjectRepository(TaskEntity)
-        private readonly taskRepository: Repository<TaskEntity>,
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly taskRepository: Repository<TaskEntity>,
 
-        private readonly contractService: ContractService,
+    private readonly contractService: ContractService,
+  ) {}
 
-    ) {}
+  async create(task: CreateTaskDto): Promise<TaskEntity> {
+    const newTask = this.taskRepository.create(task);
 
-    async create(task: CreateTaskDto): Promise<TaskEntity> {
+    newTask.contracts = await Promise.all(
+      task.contractIds.map(
+        async contract => await this.contractService.findOneById(contract),
+      ),
+    );
 
-        const newTask = this.taskRepository.create(task)
-        
-        newTask.contracts = await Promise.all(task.contractIds.map(async (contract) => await this.contractService.findOneById(contract)));
+    return await this.taskRepository.save(newTask);
+  }
 
-        return await this.taskRepository.save(newTask)
+  async findAll(): Promise<TaskEntity[]> {
+    return await this.taskRepository.find();
+  }
+
+  async findOneById(taskId: number): Promise<TaskEntity> {
+    return await this.taskRepository.findOne({ id: taskId });
+  }
+
+  async updateOne(
+    taskId: number,
+    updateTask: DeepPartial<UpdateTaskDto>,
+    exec = true,
+  ): Promise<TaskEntity> {
+    await this.taskRepository.update({ id: taskId }, updateTask);
+    const updatedTask = await this.taskRepository.findOne(taskId);
+    if (updatedTask) {
+      if (exec) {
+        this.execContracts(taskId);
+      }
+      return updatedTask;
     }
+    throw new TaskNotFoundException(taskId);
+  }
 
-    async findAll(): Promise<TaskEntity[]> {
-        return await this.taskRepository.find();
-    }
+  async deleteOne(taskId: number): Promise<TaskEntity[]> {
+    const taskToRemove = await this.taskRepository.find({ id: taskId });
+    return await this.taskRepository.remove(taskToRemove);
+  }
 
-    async findOneById(taskId: number): Promise<TaskEntity> {
-        return await this.taskRepository.findOne({id: taskId})
-    }
+  async execContracts(taskId: number): Promise<any> {
+    const task = await this.findOneById(taskId);
+    const dateNowInsert = Date.now();
+    const deadlineInsert = new Date(task['deadline']).getTime();
 
-    async updateOne(taskId: number, updateTask: DeepPartial<UpdateTaskDto>, exec = true): Promise<TaskEntity> {
-            
-        await this.taskRepository.update({id: taskId}, updateTask);
-        const updatedTask = await this.taskRepository.findOne(taskId);
-        if (updatedTask) {
-            if (exec) {
-                this.execContracts(taskId)
-            }
-            return updatedTask
-        } 
-        throw new TaskNotFoundException(taskId);
-    }
+    const taskWithRelatedContracts = await this.taskRepository.findOne({
+      where: { id: taskId },
+      relations: ['contracts'],
+    });
+    const contracts = classToPlain(taskWithRelatedContracts)['contracts'];
 
-    async deleteOne(taskId: number): Promise<TaskEntity[]> {
-        const taskToRemove = await this.taskRepository.find({id: taskId});
-        return await this.taskRepository.remove(taskToRemove);
-    }
+    const emitEvents = [];
+    contracts.forEach(contract => {
+      const executeResult = eval(
+        contract['script']
+          .replace("'${dateNowInsert}'", `\'${dateNowInsert}\'`)
+          .replace("'${deadlineInsert}'", `\'${deadlineInsert}\'`),
+      );
 
-    async execContracts(taskId: number): Promise<any> {    
-        const task = await this.findOneById(taskId)
-        const dateNowInsert = Date.now();
-        const deadlineInsert = new Date(task["deadline"]).getTime();
-        
-        const taskWithRelatedContracts = await this.taskRepository.findOne({ where: {id: taskId}, relations: ["contracts"]});
-        const contracts = classToPlain(taskWithRelatedContracts)["contracts"]
-        
-        const emitEvents = []
-        contracts.forEach( (contract) => {
-            const executeResult = eval(contract["script"]
-            .replace("\'${dateNowInsert}\'", `\'${dateNowInsert}\'`)
-            .replace("\'${deadlineInsert}\'", `\'${deadlineInsert}\'`))
+      emitEvents.push(...executeResult);
+    });
 
-            emitEvents.push(...executeResult)
-        })
+    const functionsByEmited = {
+      просрочено: () => {
+        const updateTask = {
+          warning: true,
+        };
+        this.updateOne(taskId, updateTask, false);
+      },
+      новая_задача: () => {
+        const newTask = {
+          title: 'Новая Задача',
+          desc: 'Задача созданная через контракт',
+          deadline: new Date('2020-12-30 10:59:14.664926'),
+          workerId: [521, 787],
+          createdById: 1,
+          contractIds: [],
+        };
+        this.create(newTask);
+      },
+    };
 
-        const functionsByEmited = {
-            'просрочено': () => {
-                const updateTask = {
-                    "warning": true
-                }
-                this.updateOne(taskId, updateTask, false);
-            }
-            }
-
-        emitEvents.forEach((event) => {
-            functionsByEmited[event]()
-        });
-    }
+    emitEvents.forEach(event => {
+      functionsByEmited[event]();
+    });
+  }
 }
